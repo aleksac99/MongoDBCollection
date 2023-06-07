3a7159aba9ed26b8921fdf087e7b0943623f8d15
aleksac99 <aleksa.cvetanovic99@gmail.com>
2023-06-07 10:48:23 +0200

    Create query_student_by_value

--- src/operators/comparison_operators.py
+++ src/operators/comparison_operators.py
@@ -1,11 +1,11 @@
+import sys
+sys.path.append('..')
+
 from utils import get_db
 import pprint
 
-def query_student(operator, value):
+def query_student_by_value(field: str, value: int | str) -> None:
 
     db = get_db()
-    res = db.students.find_one({"first_name": "Mike"})
+    res = db.students.find_one({field: value})
     pprint.pprint(res)
-
-if __name__=="__main__":
-    query_student(1,1)
--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 
 
 def get_db():
-    client = MongoClient('localhost', '27017')
+    client = MongoClient('localhost', 27017)
     return client.mock_database
