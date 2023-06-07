a51c6fef26a350c014347744912b0c89fff28c89
aleksac99 <aleksa.cvetanovic99@gmail.com>
2023-06-07 11:30:58 +0200

    Start working on call_batch method

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,10 @@ from pymongo import MongoClient
 def get_db():
     client = MongoClient('localhost', 27017)
     return client.mock_database
+
+
+def call_batch(batch: list[dict]) -> None:
+
+    for b in batch:
+        b['function'](b['parameters'])
+        print("---")
