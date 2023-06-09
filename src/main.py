2d99e137a9ee2fbc0c2061e12cf24502c17deb8f
aleksac99 <aleksa.cvetanovic99@gmail.com>
2023-06-09 06:30:49 +0200

    Change main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import pprint
 from utils import get_db
 
-
-from operators.query_and_projection import *
+# from operators.query_and_projection import *
+from operators.aggregation import *
 
 if __name__=="__main__":
 
     db = get_db()
 
-    pprint.pprint(q13(db))
+    pprint.pprint(q7(db))
