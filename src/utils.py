833c261cee8753b6771081f7099e1398c6ae1b76
aleksac99 <aleksa.cvetanovic99@gmail.com>
2023-06-09 13:18:33 +0200

    Remove unused function from utils

--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,3 @@ from pymongo import MongoClient
 def get_db():
     client = MongoClient('localhost', 27017)
     return client.mock_db
-
-
-def call_batch(batch: list[dict]) -> None:
-
-    for b in batch:
-        b['function'](b['parameters'])
-        print("---")
